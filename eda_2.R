cb026c77e9c6ea16fdcaab2382f30b0fdff60c73
basselus <Mozes-live@hotmail.fr>
2020-03-03 22:23:45 +0100

    Exploratory data analysis

---
+++ eda_2.R
@@ -0,0 +1,253 @@
+
+#***************************************************************************
+#*********************** Load Necessary Packages ***************************
+#***************************************************************************
+
+
+packages = c("funModeling","tidyverse","caret","dplyr")
+
+package_check <- lapply(packages, FUN = function(x){
+  
+  if (!require(x, character.only=T)) {
+    
+    install.packages(x, dependencies = T)  
+    library(x, character.only=T)
+  }
+  
+})
+
+
+#***************************************************************************
+#*********************** Load Dataset **************************************
+#***************************************************************************
+
+
+
+data=read.delim("https://raw.githubusercontent.com/pablo14/data-integrity/master/messy_data.txt", sep = ';')
+di=data_integrity(data)
+summary(di)
+
+status=df_status(heart_disease, print_results = F)
+status
+
+
+#***************************************************************************
+#*********************** Start exploring the data **************************
+#***************************************************************************
+
+
+library(funModeling)
+library(dplyr)
+data(heart_disease)
+
+
+#***************************************************************************
+#*************************** UNIVARIATE ANALYSIS ***************************
+#***************************************************************************
+
+
+#***************************************************************************
+#**** 1 - Checking missing values, zeros, data type, and unique values *****
+#***************************************************************************
+
+# SUMMARY FUNCTIONS : describe is in the Hmisc package and all the rest is from funmODELLING
+# df_status(data): Profiling dataset structure
+# describe(data): Numerical and categorical profiling (quantitative)
+# freq(data): Categorical profiling (quantitative and plot).
+# profiling_num(data): Profiling for numerical variables (quantitative)
+# plot_num(data): Profiling for numerical variables (plots)
+
+
+# call df_status function in Funmodelling package to search for missing values
+
+df_status(heart_disease)
+
+#Legend :
+
+  # q_zeros: quantity of zeros (p_zeros: in percent)
+  # q_inf: quantity of infinite values (p_inf: in percent)
+  # q_na: quantity of NA (p_na: in percent)
+  # type: factor or numeric
+  # unique: quantity of unique values
+
+#***************************************************************************
+#************************ 2 - Filtering unwanted cases *********************
+#***************************************************************************
+
+  # Removing variables with a high number of zeros
+  # Profiling the Data Input
+
+  my_data_status=df_status(heart_disease, print_results = F)
+  
+  # Removing variables with 60% of zero values
+  vars_to_remove = filter(my_data_status, p_zeros > 60)  %>% .$variable
+  vars_to_remove
+
+  # Keeping all columns except the ones present in 'vars_to_remove' vector
+  heart_disease_2 = select(heart_disease, -one_of(vars_to_remove))
+
+  
+#***************************************************************************
+#***************** 3 - Ordering data by percentage of zeros*****************
+#***************************************************************************    
+
+  
+  arrange(my_data_status, -p_zeros) %>% select(variable, q_zeros, p_zeros)
+
+  
+#***************************************************************************
+#*********************** 4 - Getting to know the data***********************
+#***************************************************************************    
+  
+  describe(heart_disease)
+  dim(heart_disease)
+  colnames(heart_disease)  
+  glimpse(heart_disease)  
+  str(heart_disease)
+    
+  # Use the describe function
+  heart_disease_3 = select(heart_disease, thal, chest_pain)
+  describe(heart_disease_3)
+  
+  
+  # n: quantity of non-NA rows. In this case, it indicates there are 301 patients containing a number.
+  # missing: number of missing values. Summing this indicator to n gives us the total number of rows.
+  # unique: number of unique (or distinct) values.
+  
+  
+#***************************************************************************
+#***************** 5 - Profiling categorical variables *********************
+#***************************************************************************    
+  
+  freq(data = heart_disease, input = c('thal', 'chest_pain'))
+  
+  #if input is missing, then it will run for all factor or character variables present in a given data frame:
+  
+  freq(data = heart_disease_2)
+  
+  # remove plots and NAs 
+  freq(data=heart_disease$thal, plot = FALSE, na.rm = TRUE)
+
+  # Add parameters to export plots
+  freq(data=heart_disease, na.rm = TRUE, path_out = "D:/coding/rstudio/data science_projects")
+  
+#***************************************************************************
+#***************** 6 - Profiling numerical variables ***********************
+#***************************************************************************   
+
+    # using world bank data
+  
+  library("Hmisc")
+  # Loading data from the book repository without altering the format
+  data_world=read.csv(file = "https://goo.gl/2TrDgN", header = T, stringsAsFactors = F, na.strings = ".." )
+  
+  # Excluding missing values in Series.Code. The data downloaded from the web page contains four lines with "free-text" at the bottom of the file.
+  
+  vides = which(data_world$Series.Code %in% c(""))
+  subset_example = subset(data_world, data_world$Series.Code=="", c(1,3))
+  data_world=filter(data_world, Series.Code!="")
+  
+  
+  # The magical function that keeps the newest values for each metric.
+  # The magical function that keeps the newest values for each metric. If you're not familiar with R, then skip it.
+  max_ix<-function(d) 
+  {
+    ix=which(!is.na(d))
+    res=ifelse(length(ix)==0, NA, d[max(ix)])
+    return(res)
+  }
+  
+  data_world$newest_value=apply(data_world[,5:ncol(data_world)], 1, FUN=max_ix)
+  
+  # Printing the first three rows
+  head(data_world, 3)
+  
+  # Get the list of indicator descriptions.
+  names=unique(select(data_world, Series.Name, Series.Code))
+  head(names, 5)
+  
+  #Translate new indicators into clearer ones 
+  # Convert a few
+  df_conv_world=data.frame(
+    new_name=c("urban_poverty_headcount", 
+               "rural_poverty_headcount", 
+               "gini_index", 
+               "pop_living_slums",
+               "poverty_headcount_1.9"), 
+    Series.Code=c("SI.POV.URHC", 
+                  "SI.POV.RUHC",
+                  "SI.POV.GINI",
+                  "EN.POP.SLUM.UR.ZS",
+                  "SI.POV.DDAY"), 
+    stringsAsFactors = F)
+  
+  # adding the new indicator value
+  data_world_2 = left_join(data_world, 
+                           df_conv_world, 
+                           by="Series.Code", 
+                           all.x=T)
+  
+  data_world_2 = 
+    mutate(data_world_2, Series.Code_2=
+             ifelse(!is.na(new_name), 
+                    as.character(data_world_2$new_name), 
+                    data_world_2$Series.Code)
+    )
+  
+  
+  # The package 'reshape2' contains both 'dcast' and 'melt' functions
+  library(reshape2)
+  
+  data_world_wide=dcast(data_world_2, Country.Name  ~ Series.Code_2, value.var = "newest_value")
+  
+  
+# DOING THE NUMERICAL PROFILING IN R
+  
+  library(Hmisc) # contains the `describe` function
+  
+  
+  vars_to_profile=c("gini_index", "poverty_headcount_1.9")
+  data_subset=select(data_world_wide, one_of(vars_to_profile))
+  
+  # Using the `describe` on a complete dataset. # It can be run with one variable; for example, describe(data_subset$poverty_headcount_1.9)
+  
+  describe(data_subset)
+  
+  
+  profiling_num(data_subset)
+  profiling_num(data_world_wide)
+  
+  
+  #To avoid printing all the results we can make use a select filter from dplyr
+  
+  profiling_table = profiling_num(data_world_wide) %>% select(variable, mean, p_01, p_99, range_80)
+  profiling_table  
+
+  
+  # PROFILING NUMERICAL VARIABLES BY PLOTTING
+  
+  plot_num(data_subset)
+  plot_num(data_world_wide)
+  
+  # Adjusting the number of bars using bins parameter
+  
+  plot_num(data_world_wide , bins = 20)
+  
+  
+  #***************************************************************************
+  #*************************** MULTIVARIATE ANALYSIS *************************
+  #***************************************************************************
+  
+  
+  libs = c("minerva","ggplot2","gridExtra","scipen=999")
+  
+  package_check <- lapply(libs, FUN = function(x){
+    
+    if (!require(x, character.only=T)) {
+      
+      install.packages(x, dependencies = T)  
+      library(x, character.only=T)
+    }
+    
+  })
+  
---
+++ eda_3.R
@@ -0,0 +1,479 @@
+
+# Chargement des packages
+
+packages = c("prettyR","gplots","binom","Epi", "psych")
+
+package_check <- lapply(packages, FUN = function(x){
+  
+  if (!require(x, character.only=T)) {
+    
+    install.packages(x, dependencies = T)  
+    library(x, character.only=T)
+  }
+  
+})
+
+
+# chargement des fichiers de données 
+
+
+sp=read.csv("sp1.csv", header = T, sep = ";", stringsAsFactors = T)
+dim(sp)
+str(sp)
+attach(sp)
+
+sp_2=read.csv("sp2.csv", header = T, sep = ";", stringsAsFactors = T)
+dim(sp_2)
+str(sp_2)
+attach(sp_2)
+
+
+repdat=read.csv("repdat.csv", header = T, sep = ";", stringsAsFactors = T)
+dim(repdat)
+str(repdat)
+attach(repdat)
+
+
+
+# DISTRIBUTION DES VARIABLES ET REPRESENTATIONS GRAPHIQUES
+
+#Représentation graphique de professions:
+
+barplot(table(prof))
+pie(table(prof))
+
+
+#Représentation d'une variable quantitative discrète
+
+hist(age, col="blue", main = "distribution de l'age", xlab = "âge", ylab = "fréquences")
+boxplot(age, col = "lightgreen", xlab = "âge")
+
+# Les BAM sont utiles pour représenter la distribution d'une variable quantitative 
+# en fonction de sous-groupes :
+#exemple distribution de l'age en fonction de la variable rs(recherche de sensation)
+
+boxplot(age~rs, xlab="recherche de sensations", ylab="age", 
+        main="distribution de rs selon l'age", col=c("red", "yellow", "green"))
+
+
+# Distribution conjointe de 2 variables aléatoires quantitatives
+
+plot(n.enfant~age) 
+
+# Plus un détenu est âgé, plus son nombre d'enfants est élevé
+
+# Pour éviter la superposition des points on peut utiliser la plot jitter
+
+plot(jitter(age), jitter(n.enfant))
+
+
+# Représentation temporelle d'une variable aléatoire quantitative avec la fonction plotmeans
+install.packages("gplots")
+library(gplots)
+plotmeans(HDRS~VISIT, barcol = "green")
+
+# On note la progression de l'état symptomatique des patients au fil du temps
+
+# Mesure de la variabilité du phénomène d'un sujet à l'autre
+# On utilise la fonction interaction.plot:
+
+interaction.plot(VISIT, # variable temporelle
+                 NUMERO, # variable qui indique chaque sujet   
+                 HDRS,  # variable représentée
+                 lty =6, legend = FALSE)
+
+
+# MESURES DE POSITION ET DE DISPERSION
+
+# les mesures agrégées servent à synthétiser l'information (exemple moyenne et écart-type)
+# pour une variable catégorielle il s'agit de lister les pourcentages de toutes les modalités
+# pour une variable il faut prendre d'autres paramètres comme la moyenne et la médiane
+# moyenne : baricentre = sens physique
+# dans une distribution symétrique mean=median
+# dans le cas contraire il ya des différences entre les 2
+# Ecarts interquartiles
+# Dans une distribution normale , l'intervalle :[m-e.t., m+e.t.] contient approximativement 2/3 des données
+# 
+# En pratique : on utilise la fonction summary
+
+summary(sp)
+
+# Pour une visualisation plus commode on utilise la fonction describe du package (prettyR)
+
+install.packages("prettyR")
+library(prettyR)
+describe(sp) # elle ne présente pas les quartiles, le minimum et le maximum (ceux-ci permettent de détecter les valeurs aberrantes)
+
+# On peut ajouter ces paramètres avec :
+
+describe(sp, num.desc = c("mean", "sd", "median", "min", "max", "valid.n"))
+
+# GESTION DES VARIABLES DANS UN DATAFRAME
+
+# explications sur l'interface de Rstudio 
+
+# On va utiliser le fichier sp2
+
+str(sp_2)
+dim(sp_2)
+names(sp_2)
+summary(sp_2)
+summary(age)
+describe(sp_2)
+
+# Explication sur le r�le de les fonctions attach et detach
+
+sp_2$age[2]  # On afficche le deuxième individu de la variable age
+sp_2$age[3:10] # On affiche de la 1ere à la 10eme observation
+head(sp_2$age, n=10) # équivalent à la précédente
+min(sp_2$age)  # on a NA en retour car il ya des valeurs manquantes
+min(sp_2$age, na.rm = TRUE) # Option pour éviter le décompte des valeurs manquantes
+unique(sp_2$abus) # Pour afficher les niveaux ou modalités de la variable abus
+length(sp_2$abus) # nombre d'observations d'une variable
+nrow(sp_2)   # nombre de lignes
+ncol(sp_2)   # nombre de colonnes
+table(sp_2$rs, useNA = "always") # ajouter l'option usena pour s'assurer de compter les valeurs manquantes
+summary(sp_2$abus)
+
+# On va traiter la variable abus comme une variable catégorielle (factor)
+
+class(sp_2$abus)
+sp_2$abus=as.factor(as.integer(sp_2$abus))
+sp_2$abus=factor(sp_2$abus, levels = c("0","1"), labels = c("Non","Oui"))
+levels(sp_2$abus)
+table(sp_2$abus, useNA = "always")
+
+# Variable nombre d'enfants
+
+names(sp_2)
+class(sp_2$n.enfant)
+summary(sp_2$n.enfant)
+table(sp_2$n.enfant)
+table(sp_2$n.enfant > 4)
+
+# On crée une variable nombre d'enfants supérieur à 4
+
+sp_2$n.enfant.cat=factor(sp_2$n.enfant)
+levels(sp_2$n.enfant.cat)
+nlevels(sp_2$n.enfant.cat)  # vérification du nombre de niveaux
+
+# Création d'une catégorie 5 enfants et +
+
+levels(sp_2$n.enfant.cat)[6:13]<- "5+"
+table(sp_2$n.enfant.cat)
+
+# on le fait avec la fonction ifelse en créant une nouvelle variable
+
+sp_2$nenf_bis=ifelse(sp_2$n.enfant>=5, "oui", "non")
+head(sp_2$nenf_bis)
+table(sp_2$nenf_bis)
+
+# Sauvegarder le fichier en format R
+
+save(sp_2, file = "sp_v1.rda")  # il sera chargé dans notre répertoire de travail
+
+# On sauve l'historique des commandes
+
+savehistory(file = "commandes_1.R")
+
+
+# INDEXATIONS AVEC CRITERES D'OBSERVATION
+
+# Rechargement du dernier fichier de travail
+
+load("sp_v1.rda")
+names(sp_2)
+
+# indexation de la 1ere observation et de la 1ere variable
+
+sp_2[1,1]
+sp_2[1,"prof"] # obs 1 , variable prof
+sp_2[1:2,c(3,4)] # obs 1 à 2, variable 3 à 4
+
+# On cherche les professions agriculteurs
+
+head(sp_2$prof=="agriculteur")
+table(sp_2$prof=="agriculteur")  # solution 1
+
+head(sp_2$prof[which(sp_2$prof %in% c("agriculteur"))]) # solution 2 avec which
+which(sp_2$prof=="agriculteur")   # renvoi des numéros d'observation
+
+# On indexe les valeurs de la variable âge pour lesquels profession==agriculteur
+
+sp_2$age[which(sp_2$prof=="agriculteur")]
+
+# usage de la commande subset
+
+attach(sp_2)
+subset(sp_2, prof=="agriculteur", age)  # dans l'ordre la base, le filtre et la variable
+subset(sp_2, prof=="agriculteur", c(2,5)) # on étend la sélection à plusieurs variables
+subset(sp_2, prof=="agriculteur", 1:6)
+
+# Combinaison de 2 filtres (ouvriers et nombre d'enfants supérieur à 2)
+
+subset(sp_2, prof=="ouvrier" & n.enfant>2, c(age, duree, discip, n.enfant))
+
+# ajout de filtres en excluant les NA sur la variable durée
+
+subset(sp_2, prof=="ouvrier" & n.enfant>2 & complete.cases(duree), c(age, duree, discip, n.enfant))
+
+
+# On stocke les résultats de la variable nenfantcat dans une table et on effectue des opérations
+
+tab<-table(n.enfant.cat)
+sum(tab)
+tab/sum(tab)
+
+# On peut faire la même chose avec les fonctions round et prop.table
+
+round(prop.table(tab), digits = 2) 
+
+
+# Représentation graphique pour la variable catégorielle (barplot)
+
+barplot(prop.table(tab)*100, col = "blue", main = "diagramme en bâton")
+
+# On ajuste les labels verticaux
+
+barplot(prop.table(tab)*100, ylim = c(0,30), las=1,  col = "blue", main = "diagramme en bâton")
+
+
+# Représentation graphique pour la variable numérique (histogramme)
+
+summary(age)
+hist(age, col = "blue", main = "distribution de la variable age")
+
+  # Changement du nombre de classes:
+  hist(age, nclass=8, col = "blue", main = "distribution de la variable age")
+  
+  # ajout des densités non paramétriques
+  hist(age, nclass=8, probability = TRUE, las=1)
+  lines(density(sp_2$age, na.rm = TRUE))
+  
+  
+# INTERVALLES DE CONFIANCE
+  
+# passage de l'échelle de l'échantillon à celle de la population: l'objectif est de généraliser 
+  #la mesure d'un paramètre (moyenne, pourcentages, variances , etc.)
+  # si on a un paramètre avec distribution normale, le paramètre estimé par "m" et "e.t",
+  # L"intevalle de confiance  à 95% du paramètre est de :
+                  #  [m-1.96*e.t.,m+1.96*e.t.]
+  
+  library(prettyR)
+  describe(sp$age)
+  summary(sp$age)
+  borne_inf=38.9-1.96*13.28/sqrt(799)
+  borne_sup=38.9+1.96*13.28/sqrt(799)
+  borne_inf
+  borne_sup
+  
+  # Un intervalle de confiance à 95% de l'âge est compris entre 38 et 40 ans
+  
+  # Estimation automatique à l'aide de R : on estime l'intervalle de confiance en utilisant toutes les méthodes
+  
+  install.packages("binom")
+  library(binom)
+  binom.confint(3,10, methods = "all") # 3 est le nombre d'individus tirés
+                                       #  10 est la taille de l'échantillon
+                                       #  "all" spécifie de prendre toutes les méthodes d'estimation
+  
+  # Il faut choisir la méthode "exact" avec un intervalle de confiance à 7 et 65%
+  
+  # quand la taille de l'échantillon s'accroit, toutes les méthodes convergent vers la même valeur
+  
+  # On change la taille de l'échantillon et le nombre de sujets tirés pour vérifier cela
+  
+  binom.confint(300,1000, methods = "all") 
+  
+  # les estimations convergent vers des valeurs identiques
+  
+  # COEFFICIENTS DE CORRELATION
+  # La liaison entre 2 variable est dite forte quand la connaissance
+  # de la valeur de l'une donne une indication sur la valeur de l'autre
+  # degré de liaison : entre -1 et 1 (exemple : la forte majorité de la population 
+  # carcérale est d'origine noire ou arabe:
+    # 1 -corrélation entre ces deux phénomènes = oui
+    # 2 - causalité  = non (cequi reviendrait à dire que les noir et les arabes sont des délinquants))
+  
+ # r=0 ; il y a absence de corrélation sssi les 2 variables suivent une loi normale  
+  
+  
+  # Représention graphique de la liaison entre age et nombre d'enfants
+  
+  plot(jitter(sp$age), jitter(sp$n.enfant))
+  
+  # calcul de la corrélation
+  
+  cor(sp$age,sp$n.enfant, use = "complete.obs")
+  
+# LIAISON ENTRE 2 VARIABLES BINAIRES (ODDS RATIO ET RISQUE RELATIF)
+  
+  # exemple :maladie vs facteur de risque
+  # association entre évitement du danger(ed) et trouble dépressif (dep.cons)
+  names(sp)
+  sp$ed_2=ifelse(sp$ed>2, 1, 0)
+  str(sp)  
+  table(sp$ed_2, sp$ed, deparse.level = 2, useNA = "always")  
+  
+  #calcul odds ratio
+  install.packages("Epi")
+  library(Epi)
+  twoby2(1-sp$ed_2, 1-sp$dep.cons)
+
+  tab=table(sp$dep.cons)  
+  round(prop.table(tab), digits = 3) #  à 40% , nous ne sommes pas en présence d'une
+  # pathologie rare, donc inutile de faire usage de l'odds ratio . On se focalise sur le 
+  # risque relatif qui est égal à 2 : donc 2 fois plus de chance d'avoir un état dépressif
+  # quand on a un évitement du danger élevé par rapport au contraire
+
+ sp[c(20, 221, 342, 446, 531),]
+ tab=table(sp$prof)
+ round(prop.table(tab)*100, digits = 2)
+ t1=subset(sp,sp$age>=20 & sp$age<=30)
+
+
+  # TESTS STATISTIQUES
+ # valeur de p pour savoir si les phénomènes ne sont pas dus au hasard
+ # p dépend de la taille des échantillons
+ # Neyman et Pearson = on détermine le seuil de risque a priori (prise de décision importante :exemple des tests médicamenteux)
+ # Fischer = on calcule la valeur de p a posteriori (plus utilisé dans les autres cas)
+ 
+ # TEST DU CHI2
+ sp$ed_bis<-ifelse(sp$ed>2, 1, 0)
+ names(sp)
+ # On croise l'évitement du danger et le diagnostic de dépression et l'évitement du danger
+ tab=table(sp$ed_bis, sp$dep.cons, deparse.level = 2, useNA = "always")
+ tab_2=table(sp$ed_bis, sp$dep.cons, deparse.level = 2)
+ 
+  prop.table(tab_2, 1) # On édite le % de dépression selon le fait d'avoir ou non un haut niveau d'ed.
+  prop.table(tab_2, 2) # On édite le % de l'inverse
+  
+  #usage du Test de chi2 avec la fonction chisq.test
+  chisq.test(sp$ed_bis, sp$dep.cons, correct = FALSE) # correct=false Pour éviter le test de correction Robuste)
+  
+  #Avec un p=10 puisance -12, très largement inférieur à 5% : donc avec certitude, le hasard ne peut pas 
+  # à lui tout seul une telle différence de prévalence de dépression
+  
+  #Test alternatif de Fisher dans le cas où les conditions de validité du chi2 ne sont pas respectés(petits échantillons)
+  fisher.test(sp$ed_bis, sp$dep.cons)
+  
+  
+  #COMPARAISON DE DEUX MOYENNES
+  # test t de student
+  # conditions de validité : 
+    # 1: au moins 30 individus dans chaque groupe d'observations
+    # 2 :la variable étudiée doit suivre une loi normale
+    # 3 : égalité des variances entre les groupes considérés
+  # exemple : compraraison de l'âge(age) en fonction de l'évitement du danger(ed)
+  
+  # distribution de la variable age : ce n'est pas tout à fait une loi normale
+  hist(sp$age, col = "blue")
+  
+  # On essaye un diagramme de normalité
+  qqnorm(sp$age);qqline(sp$age)
+  
+  # Il ya un écart à la normalité sur la partie inférieure gauche du graphe.
+  # Test dégalité des variances (ou des ET) dans les groupes à comparer
+  by(sp$age, sp$ed_bis, sd, na.rm=T)
+  # La valeur des ET dans les groupes de la variable ed_bis est quasiment la même
+  # quand un ET est >= à 1,5 fois l'autre ET ça compromet la pertinence de l'usage du T de student
+  
+  # calcul du test t de student
+  
+  t.test(sp$age~sp$ed_bis, var.equal=TRUE)
+  # on a p>0.05 : donc on ne peut pas dire qu'il ya une différence significative d'age entre les 
+  # différents groupes de la variable ed_bis
+  
+  
+  # Si les conditions d'usage de t student ne sont pas réunies, on peut utiliser les tests
+  # de Wilcoxon ou de Man withney
+  
+  wilcox.test(sp$age, sp$ed_bis)
+
+  #TEST DE NULLITE DE CORRELATION de Pearson ENTRE 2 VARIABLES:
+    # condition: il faut que l'une au moins des 2 variables suive une loi normale
+  # Exemple : corrélation entre age et recherche de sensations
+  
+  cor.test(sp$age,sp$rs, method = "pearson")
+  # p est très faible : donc corrélation très significativement non nulle
+  # cor = -0.22 donc corrélation négative entre age et recherche de sensations
+  # int conf du coefficient de corrélation : 95% de chances que cor soit contenu entre :
+    # -0.2922516  et -0.1509579 dans la population totale
+  
+  # Test de nullité du coef de corrélation de Spearman : test basé sur les rangs des individus des deux variables
+  # Ce test est donc plus robuste car on fait la corrélation des rangs des individus des 2 vars 
+  
+  cor.test(sp$age,sp$rs, method = "spearman")
+ # le p est très petit et la corrélation est significativement négative
+  
+  #RESUME :
+    # WILCOXON : usage dans des cas de non normalité de la variable
+    # SPEARMAN : usage quand aucune des 2 vars ne suit une loi normale : a posteriori on 
+      # ne peut plus utiliser les techniques statistiques qui nécessitent la normalité des variables
+      # il faut toujours a priori vérifier la normalité de la variable avant de se lancer dans un choix de méthode
+  
+  # COMPARAISON D'UNE MOYENNE A UNE REFERENCE
+  # quand on connait la moyenne de référence d'une variable dans une population de référence
+  t.test(sp$age, mu=24)
+  
+  # TESTS APPARIES : MESURES AVANT ET APRES D'UN PHENOMENE
+  # on suit un groupe d'individus et on mesure le phénomène avant et après
+  
+  
+  mcnemar.test("variable avant", "variable après")
+  t.test("variable avant", "variable après", paired = TRUE)
+  
+  # TESTS D'ASSOCIATION : GRAPHIQUES BIVARIES
+  
+  load("sp_v1.rda")
+  table(sp_2$subst.cons)
+  tab=table(sp_2$subst.cons, sp_2$abus)  
+  prop.table(tab, margin = 1) # Les effectifs sont rapportés aux totaux lignes    
+  prop.table(tab, margin = 2) # Les effectifs sont rapportés aux totaux colonnes
+  
+# Même chose que la fonction prop.table
+xtabs(~ subst.cons+abus, sp_2)
+
+# barplot pour afficher les modalités
+barplot(xtabs(~ subst.cons+abus, sp_2), beside = TRUE)
+
+# Test du chi2 entre les 2 vars qualitatives
+res=chisq.test(tab)
+
+# Affichage des effectifs observés dans l'objet res
+res$observed
+
+# Affichage des effectifs attendus dans l'objet res
+res$expected
+
+# Test de Fisher
+fisher.test(tab)
+
+# Description du comportement de la variable age en fonction des 2 modalités de subst.cons
+tapply(sp_2$age, sp_2$subst.cons, mean, na.rm=TRUE) # comportement de l'age moyen en fonction de des deux modalités
+
+
+
+# Test de student pour comparer les moyennes d'age selon les modalités 0 et 1 de subst.cons
+t.test(sp_2$age[sp_2$subst.cons==0],sp_2$age[sp_2$subst.cons==1] , var.equal = TRUE)
+
+# Même résultat avec le t.test (on n'est pas obligé de préfixer la var avec le nom de la table)
+t.test(age~subst.cons, sp_2)
+aggregate(age~subst.cons, sp_2, mean) # age moyen distribué selon les 2 modalités de subst.cons (même chose que tapply, sauf qu'on n'a pas besoin de faire na.rm=true)
+
+# Représentation graphique et distributions conditionnelles avec boxplot
+boxplot(sp_2$age, sp_2$subst.cons, col = c("blue", "green"))
+
+library(gplots)
+
+with(sp_2, tapply(sp_2$age, sp_2$subst.cons, mean, na.rm=TRUE))
+
+
+#******************************************
+#REGRESSION LINEAIRE SIMPLE
+#******************************************
+
+# définition droite de régression : droite qui minimise la somme des carrés des distances entre y et ÿ
+plot(sp_2$age, sp_2$dur.interv)
+
+library(psych)
